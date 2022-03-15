d72a551cccf65dbab9e9f76b8004b45dd9806e31
swk23c8 <swk23c8@riseup.net>
2022-03-15 17:18:40 +0900

    Liar's Dice in nextjs

---
+++ pages/posts/first-post.js
@@ -0,0 +1,45 @@
+import Link from 'next/link'
+import Image from 'next/image'
+import Head from 'next/head'
+import Script from 'next/script'
+import Layout from '../../components/layout'
+import RollDice from '../../components/RollDice'
+
+const ImageTest = () => (
+	<Image
+		src="/images/latest.png"
+		height={144}
+		width={144}
+		alt="Hooman Shrek"
+	/>
+)
+
+
+export default function FirstPost() {
+	return (
+		<>
+			<Layout>
+				<Head>
+					<title>Liar&apos;s Dice</title>
+				</Head>
+				{/* <Script
+				src="https://connect.facebook.net/en_US/sdk.js"
+				strategy="lazyOnload"
+				onLoad={() =>
+					console.log(`script loaded correctly, window.FB has been populated`)
+				}
+			/> */}
+				<h1>First Post</h1>
+				<ImageTest />
+				<h2>
+					<Link href="/">
+						<a>Back to home</a>
+					</Link>
+				</h2>
+				<h3>
+					<RollDice/>
+				</h3>
+			</Layout>
+		</>
+	)
+}
